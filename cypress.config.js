ed1ae53c15c9771d25dfabfb159bebc8589e076e
Stefano Viacava <sviacava@talana.com>
2024-05-02 12:12:32 -0400

    inicital repository

---
+++ cypress.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+  on("file:preprocessor", browserify.default(config));
+  return config;
+}
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents,
+    baseUrl: 'https://talana.com',
+    specPattern: "cypress/e2e/features/*/*.feature",
+    watchForFileChanges: false,
+    chromeWebSecurity: false,
+    experimentalModifyObstructiveThirdPartyCode: true,
+    defaultCommandTimeout: 10000
+  },
+});
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//     specPattern: "cypress/e2e/features/*/*.feature",
+//     baseUrl: "https://qa-remuchile.talana.dev",
+//     chromeWebSecurity: false,
+//     env: {
+//       file: "cypress.env.json",
+//     },
+//     video: false,
+//     retries: 0,
+//     viewportWidth: 1920,
+//     viewportHeight: 1080,
+//   },
+  
+// });
