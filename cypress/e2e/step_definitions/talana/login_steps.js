219b1a88709630308250e7a99ff1b8be25969145
mcastro <mcastro@talana.com>
2024-06-04 16:10:22 -0500

    usando metodo path en login

--- cypress/e2e/step_definitions/talana/login_steps.js
+++ cypress/e2e/step_definitions/talana/login_steps.js
@@ -1,5 +1,6 @@
 import { Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
 import {loginPage} from "../../../support/pages/talana/LoginPage";
+import {generalUtils} from "../../../support/utils/GeneralUtils"; 
 
 Given("I enter in to the login from page", () => {
   cy.visit(Cypress.env('baseUrl'));
@@ -10,5 +11,5 @@ When("I login with the valid username", () => {
 });
 
 Then("I should be successfully logged in", () => {
-  cy.url().should('include', '/remuneraciones');
+  generalUtils.validatePathUrlOfPageLoaded('remuneraciones'); 
 });
