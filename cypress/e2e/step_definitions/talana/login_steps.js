ead78657a936fcc6355359f40daa6f5a732ccc78
mcastro <mcastro@talana.com>
2024-06-04 16:11:27 -0500

    dando algo de espacio entre metodos

--- cypress/e2e/step_definitions/talana/login_steps.js
+++ cypress/e2e/step_definitions/talana/login_steps.js
@@ -2,13 +2,16 @@ import { Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
 import {loginPage} from "../../../support/pages/talana/LoginPage";
 import {generalUtils} from "../../../support/utils/GeneralUtils"; 
 
+
 Given("I enter in to the login from page", () => {
   cy.visit(Cypress.env('baseUrl'));
 });
 
+
 When("I login with the valid username", () => {
   loginPage.completeLogin();
 });
+
 
 Then("I should be successfully logged in", () => {
   generalUtils.validatePathUrlOfPageLoaded('remuneraciones'); 
