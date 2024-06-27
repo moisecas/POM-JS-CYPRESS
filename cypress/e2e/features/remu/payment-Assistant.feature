44774151802c830a450f1ffe2bbe9dde03469671
mcastro <mcastro@talana.com>
2024-06-27 11:12:33 -0500

    Ajustes solicitados para la feature y steps

--- cypress/e2e/features/remu/payment-Assistant.feature
+++ cypress/e2e/features/remu/payment-Assistant.feature
@@ -1,18 +1,12 @@
-Feature: Generate Liquidation
+Feature: Add employees to salary payment assistant
 
 Background: Successful Login
     Given I enter in to the login from page
     And I login with the valid username
     And I should be successfully logged in
 
-Scenario: Open the payment page
-    When I go to the payments assistant menu option
-    And I go to the salary option
-    Then I should be loaded into the payments page
-
 Scenario: Add all employees without terminated employees option in salary Page
-    Given I am on the payments page
+    And I am on the payments page
     And the switch include terminated is off
     When I click on Add all button for add all employees
     Then should be add all without terminated employees
-
--- cypress/e2e/step_definitions/remu/payment-Assistant.js
+++ cypress/e2e/step_definitions/remu/payment-Assistant.js
@@ -2,18 +2,6 @@ import { Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
 import { sideBarPage } from "../../../support/pages/talana/SideBar";
 import { paymentAssistantPage } from "../../../support/pages/Remu/PaymentsAssistantPage"; 
 
-When("I go to the payments assistant menu option", () => {
-    sideBarPage.enterToPaymentAssistant();
-});
-
-When("I go to the salary option", () => {
-    paymentAssistantPage.goToSalary();
-});
-
-Then("I should be loaded into the payments page", () => {
-    paymentAssistantPage.pageBreadCrumbs();
-});
-
 Given("I am on the payments page", () => {
     sideBarPage.enterToPaymentAssistant();
     paymentAssistantPage.goToSalary();
@@ -24,10 +12,9 @@ Then("the switch include terminated is off", () => {
 });
 
 When("I click on Add all button for add all employees", () => {
-    paymentAssistantPage.addAllEmployees(); 
-   
+    paymentAssistantPage.addAllEmployees();
 });
 
 Then("should be add all without terminated employees", () => {
-    paymentAssistantPage.verifyMessage(); 
+    paymentAssistantPage.verifyMessage();
 });
