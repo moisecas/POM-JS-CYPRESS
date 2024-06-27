133de76c98ed7849d58c2ddd58b8962cb995aa0b
mcastro <mcastro@talana.com>
2024-06-27 14:24:13 -0500

    Cambio de nombre

--- cypress/e2e/step_definitions/remu/payment-Assistant.js
+++ cypress/e2e/step_definitions/remu/payment-Assistant.js
@@ -12,4 +12,4 @@ When("I click on Add all button for add all employees", () => {
 
 Then("should be add all without terminated employees", () => {
     paymentAssistantPage.verifyMessage();
-});
+}); 
