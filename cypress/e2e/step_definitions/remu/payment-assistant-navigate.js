9f16cf8f03af66a63fa76168417a3a6f3a93e4c9
mcastro <mcastro@talana.com>
2024-06-27 15:40:54 -0500

    Ajuste de espacios

--- cypress/e2e/step_definitions/remu/payment-assistant-navigate.js
+++ cypress/e2e/step_definitions/remu/payment-assistant-navigate.js
@@ -2,7 +2,6 @@ import { Given, When, Then} from '@badeball/cypress-cucumber-preprocessor';
 import { sideBarPage } from "../../../support/pages/talana/SideBar";
 import { paymentAssistantPage } from "../../../support/pages/Remu/PaymentsAssistantPage"; 
 
-
 When("I go to the payments assistant menu option", () => {
     sideBarPage.enterToPaymentAssistant();
 });
