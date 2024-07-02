b78f963b2aaec0e6d78e5b2c4a1b38f5777c0dad
mcastro <mcastro@talana.com>
2024-07-02 16:45:26 -0500

    Identificación de botones hasta el botón de imprimir boletas

--- cypress/support/pages/Remu/PaymentsAssistantPage.js
+++ cypress/support/pages/Remu/PaymentsAssistantPage.js
@@ -9,7 +9,15 @@ class PaymentsAssistantPage {
         searchEmployeeInput: () => cy.get('[data-cy="talana-fragmentAddPeople-inputSearchPeople"]'),
         step1NextButton: () => cy.get('[data-cy="rem-paymentAssistantSalary-step1-btnNext"]'),
         showStep1: () => cy.get('[data-cy="rem-paymentAssistant-showStep1"]'),
-        succesMessage: () => cy.get('.bootstrap-growl')            
+        succesMessage: () => cy.get('.bootstrap-growl'),
+        //pasos nuevos
+        buttonStepOne: () => cy.get('[data-cy="rem-paymentAssistantSalary-step1-btnNext"]'), //primera flecha verde
+        buttonStepOneConfirmation: () => cy.get('[data-cy="rem-paymentAssistantSalary-step1-modalError-btnGenerateLiquidation"]'), //botón modal de confirmación
+        buttonStepTwo: () => cy.get('[data-cy="rem-paymentAssistantSalary-step2-btnNext"]'), //segunda flecha verde
+        modalGenerate: () => cy.get('[data-cy="rem-modalProgressCounter-textTitle"]'), //title: Generando Boletas
+        buttonStepThree: () => cy.get('[data-cy="rem-paymentAssistantSalary-step3-btnNext"]'), //tercera flecha verde   
+        //se repite en este paso  modalGenerate title: Generando boletas
+        butoonStepFour: () => cy.get('[data-cy="rem-paymentAssistant-step4-btnPrintSettlements"]'), //title: Imprimir boletas
     }
 
     goToSalary(business) {
