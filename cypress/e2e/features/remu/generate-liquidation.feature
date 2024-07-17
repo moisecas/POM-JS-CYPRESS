a8fce7a9cc5ffa0435fcdf357d9aee88314bab3a
mcastro <mcastro@talana.com>
2024-07-17 09:07:17 -0500

    mejora feature

--- cypress/e2e/features/remu/generate-liquidation.feature
+++ cypress/e2e/features/remu/generate-liquidation.feature
@@ -1,8 +1,14 @@
 Feature: Generate liquidation 
 
 Background:
-     Given I am logged in and prepared to generate liquidation
+    Given I am logged in and on the payments assistant page
+    When I have added all employees without terminated ones
 
 Scenario: Generate liquidation for employees 
-    When I complete the liquidation steps
-    Then I should visualize the print liquidations button 
+    When I click on the step one
+    And I confirm the generation of payroll liquidations
+    And I click on the step two
+    And I see the payroll liquidation processing window
+    And I click on the step three
+    Then I should see the payroll generation processing window
+    And I visualize the print liquidations button
--- cypress/e2e/step_definitions/remu/generate-liquidation.js
+++ cypress/e2e/step_definitions/remu/generate-liquidation.js
@@ -4,26 +4,44 @@ import { loginPage } from "../../../support/pages/talana/LoginPage";
 import { paymentAssistantPage } from "../../../support/pages/Remu/PaymentsAssistantPage";
 import { dashboardPage } from "../../../support/pages/talana/DashboardPage";
 
-Given("I am logged in and prepared to generate liquidation", () => {
+Given("I am logged in and on the payments assistant page", () => {
     cy.visit(Cypress.env("baseUrl"));
     loginPage.completeLogin();
-    dashboardPage.verifyLogin(); 
+    dashboardPage.verifyLogin(); // Verifica el login y la carga del dashboard
     sideBarPage.enterToPaymentAssistant();
-    paymentAssistantPage.goToSalary();  
-    paymentAssistantPage.verifyTerminatedEmployeesOff();  
-    paymentAssistantPage.addAllEmployees();  
-    paymentAssistantPage.verifyMessage('Se agregaron'); 
+    paymentAssistantPage.goToSalary(); // Navega a la opción de sueldos
 });
 
-When("I complete the liquidation steps", () => {
+When("I have added all employees without terminated ones", () => {
+    paymentAssistantPage.verifyTerminatedEmployeesOff(); // Verifica que el switch esté apagado
+    paymentAssistantPage.addAllEmployees(); // Añade todos los empleados
+    paymentAssistantPage.verifyMessage('Se agregaron'); // Verifica el popup de confirmación
+});
+
+When("I click on the step one", () => {
     paymentAssistantPage.clickButtonStepOne();
+});
+
+When("I confirm the generation of payroll liquidations", () => {
     paymentAssistantPage.clickButtonStepOneConfirmation();
+});
+
+When("I click on the step two", () => {
     paymentAssistantPage.clickButtonStepTwo();
+});
+
+When("I see the payroll liquidation processing window", () => {
     paymentAssistantPage.verifyModalGenerate();
+});
+
+When("I click on the step three", () => {
     paymentAssistantPage.clickButtonStepThree();
+});
+
+Then("I should see the payroll generation processing window", () => {
     paymentAssistantPage.verifyModalGenerateTwo();
 });
 
-Then("I should visualize the print liquidations button", () => {
+Then("I visualize the print liquidations button", () => {
     paymentAssistantPage.clickButtonStepFour();
 });
