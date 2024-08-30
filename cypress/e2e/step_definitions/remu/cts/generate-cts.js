a78451d17d1fbaaba6184d9b0e45b1e375d526f3
mcastro <mcastro@talana.com>
2024-08-30 11:13:16 -0500

    agrego ;

--- cypress/e2e/step_definitions/remu/cts/generate-cts.js
+++ cypress/e2e/step_definitions/remu/cts/generate-cts.js
@@ -2,14 +2,14 @@ import { When, Then} from '@badeball/cypress-cucumber-preprocessor';
 import { paymentAssistantCtsPage } from '../../../../support/pages/Remu/cts/PaymentAssistantCtsPage';  
 
 When("I want to generate the payroll CTS", () => {
-    paymentAssistantCtsPage.clickButtonOne()
+    paymentAssistantCtsPage.clickButtonOne();
 });
 
 Then("I visualize the progress window to continue with the generation CTS", () => {
-    paymentAssistantCtsPage.verifyProgressStatus()
+    paymentAssistantCtsPage.verifyProgressStatus();
 }); 
 
 Then("I confirm the generation of payroll CTS {string}", (result) => {
-     paymentAssistantCtsPage.pageBreadCrumbsResult(result) 
-     paymentAssistantCtsPage.clickButtonPrint() 
+     paymentAssistantCtsPage.pageBreadCrumbsResult(result); 
+     paymentAssistantCtsPage.clickButtonPrint(); 
 });
