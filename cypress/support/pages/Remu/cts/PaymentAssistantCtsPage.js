9de742138ec9d567c7c35078cd7fe89ee8fd7bfe
mcastro <mcastro@talana.com>
2024-09-04 14:46:11 -0500

    Ticket QA 166

--- cypress/support/pages/Remu/cts/PaymentAssistantCtsPage.js
+++ cypress/support/pages/Remu/cts/PaymentAssistantCtsPage.js
@@ -3,7 +3,6 @@ class PaymentAssistantCtsPage {
         breadCrumbs: () => cy.get('strong'), 
         switchButton: () => cy.get('.ts-helper'),
         addAllEmployeesButton: () => cy.get('[data-cy="talana-fragmentAddPeople-btnAddAll"]'),
-        includeTerminatedEployeesOption: () => cy.get('.ts-helper'),
         modalProgressCounter: () => cy.get('.swal2-modal'),
         succesMessage: () => cy.get('.bootstrap-growl'),
         buttonStepOne: () => cy.get('[data-cy="rem-paymentAssistantCTS-step1-btnNext"]'), 
@@ -29,35 +28,6 @@ class PaymentAssistantCtsPage {
 
     visualModalAddAllEmployees(message) {
         this.elements.succesMessage().should('contain', message) 
-    }
-
-    clickButtonOne() {
-        this.elements.buttonStepOne().click()
-    }
-
-    verifyProgressStatus() {
-        cy.intercept('GET', '**/es/celeryapi/status/*').as('progressStatus');
-        this.elements.modalProgress().should('contain', 'Generando Documento');
-        cy.wait('@progressStatus', { timeout: 60000 }).its('response.statusCode').should('eq', 200);
-    }
-    
-    pageBreadCrumbsResult(result) { 
-        const verifyBreadCrumbs = () => {
-            cy.get('body').then(($body) => {
-                const breadCrumbs = $body.find('strong');
-                if (breadCrumbs.length === 0 || !breadCrumbs.text().includes(result)) {
-                    cy.wait(1000); 
-                    verifyBreadCrumbs(); 
-                } else {
-                    cy.get('strong').should('contain', result);
-                }
-            });
-        };    
-        verifyBreadCrumbs();
-    }
-    
-    clickButtonPrint() {
-        this.elements.buttonPrint().click() 
     }
 
 }
